3049e28f82f08d857e2653f5abb4860263ad45e8
Henrique Crispino <henrique.crispino@sptech.school>
2023-10-27 14:23:40 -0300

    Correção de bugs do carrossel de imagens do index.html

--- script.js
+++ script.js
@@ -2,12 +2,31 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   var contador_carrossel = 1;
+  var intervalo_carrossel;
+
   const radio_ids = ["radio_1", "radio_2", "radio_3", "radio_4"];
   document.getElementById(radio_ids[0]).checked = true;
 
-  setInterval(function () {
-    proxima_img_carrossel();
-  }, 10000);
+  function iniciar_carrossel() {
+    intervalo_carrossel = setInterval(function () {
+      proxima_img_carrossel();
+    }, 10000);
+  }
+
+  iniciar_carrossel();
+
+  function selecionar_imagem(indice) {
+    clearInterval(intervalo_carrossel);
+    contador_carrossel = indice;
+    document.getElementById(radio_ids[indice - 1]).checked = true;
+    iniciar_carrossel();
+  }
+
+  radio_ids.forEach(function (radio_id, indice) {
+    document.getElementById(radio_id).addEventListener("click", function () {
+      selecionar_imagem(indice + 1);
+    });
+  });
 
   function proxima_img_carrossel() {
     contador_carrossel++;
@@ -23,13 +42,13 @@ document.addEventListener("DOMContentLoaded", function () {
 /*CITAÇÕES*/
 
 function esconder_citacoes(image) {
-  var citacoes = image.parentNode.querySelector('.citacoes');
-  citacoes.style.display = 'none';
+  var citacoes = image.parentNode.querySelector(".citacoes");
+  citacoes.style.display = "none";
 }
 
 function mostrar_citacoes(image) {
-  var citacoes = image.parentNode.querySelector('.citacoes');
-  citacoes.style.display = 'block';
+  var citacoes = image.parentNode.querySelector(".citacoes");
+  citacoes.style.display = "block";
 }
 
 /*FIM CITAÇÕES*/
