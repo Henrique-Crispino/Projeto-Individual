2857f1a9a68e21caedb975c5fd19d6d18c23b535
Henrique-Crispino <henrique.crispino@sptech.school>
2023-11-29 10:01:45 -0300

    Mudanças no script de dados e nome da documentação

--- banco de dados/script-tabelas.sql
+++ banco de dados/script-tabelas.sql
@@ -63,32 +63,60 @@ constraint fkUsuario foreign key (fkUsuario) references usuario (idUsuario),
 constraint fkQuiz foreign key (fkQuiz) references quiz (idQuiz)
 );
 
+-- Seleciona todos usuarios
 select * from usuario;
 
-SELECT email, idUsuario, nome, fkRaca FROM usuario WHERE email = 'asdadw@' AND senha = 'weofbqwubfyqrg';
+-- Seleciona todas as tentativas
+select * from tentativas;
+
+-- Seleciona o email, Id do usuario, Nome do usuario e o Id da sua raça preferida de um usuário específico
+SELECT email, idUsuario, nome, fkRaca FROM usuario WHERE email = 'admin@admin.com' AND senha = 'admin@admin';
         
+-- Seleciona o nome e a quantidade de usuários que as raças de cavalo preferidas tem 
 SELECT r.nome, COUNT(u.idUsuario) as quantidade
 FROM usuario u
 JOIN raca r ON u.fkRaca = r.idRaca
 GROUP BY r.nome, u.fkRaca
 order by quantidade desc;
 
+-- Seleciona o total de acertos e erros fáceis que um usuário específico tem no total
 select sum(t.acertos_facil) as acertosFaceis, sum(t.erros_facil) as errosFaceis 
 from tentativas t
 join usuario u on t.fkUsuario = u.idUsuario
 where u.idUsuario = 1;
 
+-- Seleciona o total de acertos e erros Médios que o usuário específico tem no total
 select sum(t.acertos_medio) as acertosMedios, sum(t.erros_medio) as errosMedios 
 from tentativas t
 join usuario u on t.fkUsuario = u.idUsuario
 where u.idUsuario = 1;
 
+-- Seleciona o total de acertos e erros Difíceis que o usuário específico tem no total
 select sum(t.acertos_dificil) as acertosDificeis, sum(t.erros_dificil) as errosDificeis 
 from tentativas t
 join usuario u on t.fkUsuario = u.idUsuario
 where u.idUsuario = 1;
 
+-- Seleciona o total de acertos e erros que o usuário específico tem no total
 select sum(t.acertos_geral) as totalAcertos, sum(t.erros_geral) as totalErros
 from tentativas t
 join usuario u on t.fkUsuario = u.idUsuario
 where u.idUsuario = 1;
+
+-- Seleciona todos os tipos de acertos e erros que um usuário específico tem no total
+SELECT 
+	u.nome as NomeUsuario,
+    SUM(t.acertos_facil) as acertosFaceis, 
+    SUM(t.erros_facil) as errosFaceis,
+    SUM(t.acertos_medio) as acertosMedios, 
+    SUM(t.erros_medio) as errosMedios,
+    SUM(t.acertos_dificil) as acertosDificeis, 
+    SUM(t.erros_dificil) as errosDificeis,
+    SUM(t.acertos_geral) as totalAcertosGerais, 
+    SUM(t.erros_geral) as TotalErrosGerais
+FROM 
+    tentativas t
+JOIN 
+    usuario u ON t.fkUsuario = u.idUsuario
+WHERE 
+    u.idUsuario = 1;
